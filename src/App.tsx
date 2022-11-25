ba6588097e009a9db876a9c5fceb23c31f8ab6e7
Ricardo Machado <ricardo_cabista@hotmail.com>
2022-11-25 11:56:55 -0300

    chore: git-message-linter adicionado, correções aplicadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
+function App () {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+
         </a>
       </div>
       <h1>Vite + React</h1>
