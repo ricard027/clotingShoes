6187ab1950947450ea53b261097a9a89a72c8006
Ricardo Machado <ricardo_cabista@hotmail.com>
2022-11-28 08:02:55 -0300

    style: estilos adicionados no component categories

--- src/categories/Categories.tsx
+++ src/categories/Categories.tsx
@@ -1,12 +1,37 @@
+import './categories.css'
+import Category from '../types/category.types'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+const url = import.meta.env.VITE_API_URL
 
-const categories = () => {
+const Categories = () => {
+  const [categories, setCategoties] = useState<Category[]>([])
+
+  const fetchCategories = async () => {
+    try {
+      const { data } = await axios.get(`${url}/api/category`)
+      setCategoties(data)
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    void fetchCategories()
+  }, [])
+
   return (
-    <div>
-        <h2>categorias</h2>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima libero a qui explicabo vero necessitatibus? Quo, eos voluptatum totam quasi consequuntur distinctio blanditiis ut, qui nisi neque facilis? Excepturi, iure.</p>
+    <div className='categories-container'>
+      <div className="categories-content">
+        {categories.map(categorieItem => (
+        <div className='category-item' key={categorieItem.id}>
+           <h3>{categorieItem.name}</h3>
+           <p>{categorieItem.displayName}</p>
+        </div>
+        ))}
+      </div>
     </div>
   )
 }
 
-export default categories
+export default Categories
