bf4f4798a327575c9f261075f18ec5b23d96b279
Ricardo Machado <ricardo_cabista@hotmail.com>
2022-11-29 15:31:35 -0300

    fix: correçoes aplicadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-//  components
-import Home from './pages/home/Home'
-import { FunctionComponent } from 'react'
 //  styles
 import './App.css'
-
+// libs
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { FunctionComponent } from 'react'
+// pages
+import Home from './pages/home/Home'
+import LoginPage from './pages/login/LoginPage'
+import SignUp from './pages/signUp/SignUp'
 const App: FunctionComponent = () => {
   return (
     <div className="App">
        <BrowserRouter>
           <Routes>
              <Route path='/' element={<Home/>}/>
+             <Route path='login' element={<LoginPage/>}/>
+             <Route path='create-acount' element={<SignUp/>}/>
           </Routes>
        </BrowserRouter>
     </div>
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,22 +1,37 @@
-import './header.css'
+// styles
+
+import { CustomHeader } from './header.style'
+// libs
 import { FiShoppingCart } from 'react-icons/fi'
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
+  const navigate = useNavigate()
+
+  const getLoginPage = () => {
+    navigate('/login')
+  }
+  const getExplorerPage = () => {
+    navigate('/')
+  }
+  const getCreateAcountPage = () => {
+    navigate('/create-acount')
+  }
   return (
-    <header className='headerComponent'>
+    <CustomHeader>
      <div className="logo">
         Cloting Store
      </div>
      <ul className='listHeader'>
-        <li className='itemListHeader'>Explorar</li>
-        <li className='itemListHeader'>Login</li>
-        <li className='itemListHeader'>Criar Conta</li>
+        <li className='itemListHeader' onClick={getExplorerPage}>Explorar</li>
+        <li className='itemListHeader' onClick={getLoginPage}>Login</li>
+        <li className='itemListHeader' onClick={getCreateAcountPage}>Criar Conta</li>
         <li className='itemListHeader cart'>
             <FiShoppingCart />
             <span className='qtdCart'>+99</span>
         </li>
      </ul>
-    </header>
+    </CustomHeader>
   )
 }
 
