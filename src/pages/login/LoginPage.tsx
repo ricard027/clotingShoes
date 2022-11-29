c25f71f27dc736d04658b839b3055c6c4c416cd6
Ricardo Machado <ricardo_cabista@hotmail.com>
2022-11-29 15:17:53 -0300

    feat: pages login e sign up adicionados

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,9 +1,49 @@
+import { BsGoogle } from 'react-icons/bs'
+import { FiLogIn } from 'react-icons/fi'
+
+// Components
+import CustomButton from '../../components/button/Custombutton'
+import CustomInput from '../../components/input/CustomInput'
+import Header from '../../components/header/header'
+
+// Styles
+import {
+  LoginContainer,
+  LoginContent,
+  LoginHeadline,
+  LoginInputContainer,
+  LoginSubtitle
+} from './loginPage.style'
 
 const LoginPage = () => {
   return (
-    <div>
-        <h1>Login</h1>
-    </div>
+    <>
+      <Header />
+
+      <LoginContainer>
+        <LoginContent>
+          <LoginHeadline>Entre com a sua conta</LoginHeadline>
+
+          <CustomButton startIcon={<BsGoogle size={18} />}>
+            Entrar com o Google
+          </CustomButton>
+
+          <LoginSubtitle>ou entre com o seu e-mail</LoginSubtitle>
+
+          <LoginInputContainer>
+            <p>E-mail</p>
+            <CustomInput placeholder="Digite seu e-mail" />
+          </LoginInputContainer>
+
+          <LoginInputContainer>
+            <p>Senha</p>
+            <CustomInput placeholder="Digite sua senha" />
+          </LoginInputContainer>
+
+          <CustomButton startIcon={<FiLogIn size={18} />}>Entrar</CustomButton>
+        </LoginContent>
+      </LoginContainer>
+    </>
   )
 }
 
---
+++ src/pages/signUp/SignUp.tsx
@@ -0,0 +1,12 @@
+// component
+import Header from '../../components/header/header'
+const Createacount = () => {
+  return (
+    <div>
+        <Header/>
+        <h3>criar conta</h3>
+    </div>
+  )
+}
+
+export default Createacount
