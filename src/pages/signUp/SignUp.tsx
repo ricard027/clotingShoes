55a63e7425105aab49f83e34af3c09c342d1b125
Ricardo Machado <ricardo_cabista@hotmail.com>
2022-12-12 21:17:31 -0300

    feat: component sign  up criado

--- src/pages/signUp/SignUp.tsx
+++ src/pages/signUp/SignUp.tsx
@@ -1,11 +1,42 @@
 // component
+import CustomButton from '../../components/button/Custombutton'
 import Header from '../../components/header/header'
+import CustomInput from '../../components/input/CustomInput'
+import { FiLogIn } from 'react-icons/fi'
+import { SignUpContainer, SignUpContent, SignUpHeadline } from './signUp.style'
 const Createacount = () => {
   return (
-    <div>
+    <>
         <Header/>
-        <h3>criar conta</h3>
-    </div>
+        <SignUpContainer >
+          <SignUpContent>
+              <SignUpHeadline> Crie sua conta </SignUpHeadline>
+
+              <SignUpContainer >
+             <p>Nome</p>
+             <CustomInput placeholder='digite seu nome'/>
+             </SignUpContainer>
+
+             <SignUpContainer>
+              <p>Sobrenome</p>
+              <CustomInput placeholder='digite seu sobrenome'/>
+             </SignUpContainer>
+
+             <SignUpContainer>
+              <p>Email</p>
+              <CustomInput placeholder='digite seu email'/>
+             </SignUpContainer>
+
+             <SignUpContainer>
+              <p>Senha</p>
+              <CustomInput placeholder='digite sua senha' type='password'/>
+             </SignUpContainer>
+
+             <CustomButton startIcon={<FiLogIn size={18} />}> Criar conta</CustomButton>
+
+          </SignUpContent>
+        </SignUpContainer>
+    </>
   )
 }
 
