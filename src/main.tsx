1ccd15b55c1a4cbf70fd354e06e0828b62773ebd
Ricardo Machado <ricardo_cabista@hotmail.com>
2023-02-10 21:43:03 -0300

    chore: redux adicionado

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+// styles
 import './index.css'
+// utilities
+import { Provider } from 'react-redux'
 import UserContextProvider from './contexts/user.context'
 import CategoriContextProvider from './contexts/category.context'
 import CartContextProvider from './contexts/cart.context'
+import store from './store/store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <Provider store={store}>
     <UserContextProvider>
       <CategoriContextProvider>
         <CartContextProvider>
@@ -15,5 +20,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         </CartContextProvider>
       </CategoriContextProvider>
     </UserContextProvider>
+    </Provider>
   </React.StrictMode>
 )
